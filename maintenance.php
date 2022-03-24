f9f98aa418c7945c374f39ea4015a01e38e4ae47
Aaron Ware <aaron@linchpin.com>
2022-03-24 14:48:53 -0400

    Update maintenance.php

--- maintenance.php
+++ maintenance.php
@@ -5,44 +5,50 @@
 		<meta name="viewport" content="width=device-width">
 		<title>Scheduled Maintenance</title>
 		<style>
-		html {
-			background: #f1f1f1;
-		}
-		body {
-			background: #fff;
-			color: #444;
-			font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-			margin: 2em auto;
-			padding: 1em 2em;
-			max-width: 700px;
-			-webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.13);
-			box-shadow: 0 1px 3px rgba(0, 0, 0, 0.13);
-		}
-		h1 {
-			border-bottom: 1px solid #5C5C5C;
-			clear: both;
-			color: #666;
-			font-size: 24px;
-			margin: 30px 0 0 0;
-			padding: 0;
-			padding-bottom: 7px;
-		}
-		#error-page {
-			margin-top: 50px;
-		}
-		#error-page p,
-		#error-page .wp-die-message {
-			font-size: 14px;
-			line-height: 1.5;
-			margin: 25px 0 20px;
-		}
-		#error-page code {
-			font-family: Consolas, Monaco, monospace;
-		}
-		ul li {
-			margin-bottom: 10px;
-			font-size: 14px;
-		}
+			html {
+				background: #fff;
+			}
+
+			body {
+				background: #fff;
+				color: #444;
+				font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+				margin: 2em auto;
+				padding: 1em 2em;
+				max-width: 700px;
+				-webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.13);
+				box-shadow: 0 1px 3px rgba(0, 0, 0, 0.13);
+			}
+
+			h1 {
+				border-bottom: 1px solid #5C5C5C;
+				clear: both;
+				color: #666;
+				font-size: 24px;
+				margin: 30px 0 0 0;
+				padding: 0;
+				padding-bottom: 7px;
+			}
+
+			#error-page {
+				margin-top: 50px;
+			}
+
+			#error-page p,
+			#error-page .wp-die-message {
+				font-size: 14px;
+				line-height: 1.5;
+				margin: 25px 0 20px;
+			}
+
+			#error-page code {
+				font-family: Consolas, Monaco, monospace;
+			}
+
+			ul li {
+				margin-bottom: 10px;
+				font-size: 14px;
+			}
 		</style>
 	</head>
 	<body id="error-page">
